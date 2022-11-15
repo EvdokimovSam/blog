94bdc9f644655ca03210cabd90b65faca9962b6c
Sam <sema.evdokimov.02@mail.ru>
2022-11-16 02:33:20 +0300

    add npm

--- resources/views/form_reg.blade.php
+++ resources/views/form_reg.blade.php
@@ -8,33 +8,33 @@
     <title>Document</title>
 </head>
 <body>
-    <form method="get" action="#">
-        <div id="active-modal__reg" class="vmodal">
-            <div class="modal-container">
-                <div class="modal-container__header">
-                    <h3 class="modal-container__title">Регистрация</h3>
-                    <span class="question">Есть аккаунт?</span>
-                    <a class="login-link" href="{{route('form_log')}}">Войти</a>
+<form method="get" action="#">
+    <div id="active-modal__reg" class="vmodal">
+        <div class="modal-container">
+            <div class="modal-container__header">
+                <h3 class="modal-container__title">Регистрация</h3>
+                <span class="question">Есть аккаунт?</span>
+                <a class="login-link" href="{{route('form_log')}}">Войти</a>
+            </div>
+
+
+            <div class="modal-container__content">
+                <div class="modal-container__content__login">
+                    <p>Логин</p>
+                    <input class="login-input" type="text" placeholder="Логин">
                 </div>
-
-
-                <div class="modal-container__content">
-                     <div class="modal-container__content__login">
-                         <p>Логин</p>
-                         <input class="login-input" type="text" placeholder="Логин">
-                     </div>
-                     <div class="modal-container__content__password" >
-                        <p>Пароль</p>
-                        <input class="login-input" type="text"placeholder="Пароль">
-                    </div>
-                </div>
-
-
-                <div class="modal-container__footer">
-                    <button id="send-reg" class="modal-reg">Зарегистрироваться</button>
+                <div class="modal-container__content__password" >
+                    <p>Пароль</p>
+                    <input class="login-input" type="text"placeholder="Пароль">
                 </div>
             </div>
+
+
+            <div class="modal-container__footer">
+                <button id="send-reg" class="modal-reg">Зарегистрироваться</button>
+            </div>
         </div>
-    </form>
+    </div>
+</form>
 </body>
 </html>
